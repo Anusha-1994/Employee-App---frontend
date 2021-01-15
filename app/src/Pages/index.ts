eac81fe7649b0343444a657f3d82aef5ca80c883
Anusha-1994 <72099899+Anusha-1994@users.noreply.github.com>
2021-01-15 12:34:21 +0000

    Delete app directory

--- app/src/Pages/index.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/extensions */
-export { default as Login } from './Login';
-export { default as ResetPassword } from './ResetPassword';
-export { default as NotFound } from './NotFound';
-export { default as Employees } from './Employees';
-export { default as SignUp } from './SignUp';
--- app/src/Services/employeeService.ts
+++
@@ -1,8 +0,0 @@
-import api from './api';
-
-class EmployeeService {
-    getEmployeeDetails = async () => api.get(`/employees`);
-    createUpdateEmployee = async (data: any) => api.put(`/employees`, data)
-}
-
-export default new EmployeeService()
